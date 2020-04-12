9a33f8c69b1f1abc4f19a8df3f572d7fe4c21ec1
Kevin Harper <kevin@kevinharper.com>
2020-04-12 16:51:46 -0700

    Fixed flake8 errors

--- website/pages/tests.py
+++ website/pages/tests.py
@@ -5,16 +5,14 @@ from django.urls import reverse
 
 # Create your tests here.
 class HomepageTests(SimpleTestCase):
-	def test_homepage_status_code(self):
-		response = self.client.get('/')
-		self.assertEqual(response.status_code, 200)
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
 
+    def test_homepage_url_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
 
-	def test_homepage_url_name(self):
-		response = self.client.get(reverse('home'))
-		self.assertEqual(response.status_code, 200)
-
-
-	def test_homepage_template(self):
-		response = self.client.get('/')
-		self.assertTemplateUsed(response, 'home.html')
+    def test_homepage_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
