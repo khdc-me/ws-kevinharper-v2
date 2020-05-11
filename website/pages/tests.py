300cb12cd7de1f6a79812996e57dc2f1342dd44a
Kevin Harper <kevin@kevinharper.com>
2020-05-11 11:47:58 -0700

    added fletch to home.html

--- website/pages/tests.py
+++ website/pages/tests.py
@@ -19,7 +19,7 @@ class HomepageTests(SimpleTestCase):
 
     def test_homepage_contains_correct_html(self):
         response = self.client.get('/')
-        self.assertContains(response, "test")
+        self.assertContains(response, "fletch")
         self.assertNotContains(
     		response, 'This line of text prevent false positive results from previous\
                             line')
