19a6a5cf39197509a21e97a7c65348cec1fef0a1
Kevin Harper <kevin@kevinharper.com>
2020-05-15 21:12:31 -0700

    Updated website/pages/tests.py

--- website/pages/tests.py
+++ website/pages/tests.py
@@ -19,7 +19,28 @@ class HomepageTests(SimpleTestCase):
 
     def test_homepage_contains_correct_html(self):
         response = self.client.get('/')
-        self.assertContains(response, "present")
+        self.assertContains(response, "Kevin")
         self.assertNotContains(
     		response, 'This line of text prevent false positive results from previous\
+                            line')
+
+
+class ExperiencePageTests(SimpleTestCase):
+    def test_experience_page_status_code(self):
+        response = self.client.get('/experience')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        response = self.client.get(reverse('experience'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_template(self):
+        response = self.client.get('/experience')
+        self.assertTemplateUsed(response, 'pages/experience.html')
+
+    def test_homepage_contains_correct_html(self):
+        response = self.client.get('/experience')
+        self.assertContains(response, "Linux")
+        self.assertNotContains(
+                response, 'This line of text prevent false positive results from previous\
                             line')
